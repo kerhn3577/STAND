f23c52de9e6e3169d365061057bf56557970ce88
Mario A. Alanis Garcia <44460940+kerhnewek@users.noreply.github.com>
2018-10-31 10:50:15 +0100

    Add files via upload

--- STAND_MODEL.py
+++ STAND_MODEL.py
@@ -9,6 +9,7 @@ plt.close("all")
 R=8.3144621
 Rerg=R*10**7
 NA=6.02*10**23
+
 #Input 
 inputdata = {}
 inputdata['Free Gibbs Micellization Energy'] = float(input("Gibbs (J/mol):"))
@@ -19,21 +20,23 @@ inputdata['Temperature'] = float(input("T (Kelvin):"))
 inputdata['Surface tension of pure solvent:'] = float(input("TS0 (mN/m):"))
 jsondata = json.dumps(inputdata)
 
-RT=R*inputdata['Temperature']
-G=(10**16/NA)*inputdata['Temperature']*(R*10**7)*1/inputdata['Minimum Area ']
-cmc=np.exp(inputdata['Free Gibbs Micellization Energy']/RT)
+Tem=inputdata['Temperature'] 
 TS0=inputdata['Surface tension of pure solvent:']
 N=inputdata['Aggregation Number']
 Gibbs=inputdata['Free Gibbs Micellization Energy']
 Lang=inputdata['Langmuir s constant']
+MinA=inputdata['Minimum Area '] 
+
+
+
+RT=R*Tem
+
+
 
 # =============================================================================
 #Equations of the model
 # =============================================================================
 
-Totalc=np.linspace(0.00001*cmc,3*cmc,100) #Total concentration of surfactant
-
-#Simulation
 
 f=open("octil.txt","r").readlines()
 conc=[] 
@@ -49,60 +52,78 @@ for i in range(0,len(List)):
 TS_exp=np.array(TS_exp)
 u_conc=np.array(u_conc)
 u_TS=np.array(u_TS)
+
+#Simulation
+#Totalc=np.linspace(0.00001*cmc,3*cmc,100) #Total concentration of surfactant
+
+
+
 class STAND:
      
     def SEoSLang(F,b,G):
-        return TS0-G*np.log(b*F+1)
-    def Langiso(F,b):
+        return TS0-G*np.log(b*F+1) #Langmuir's Equation of State
+    
+    def Langiso(F,b): #Langmuir's Isotherm
         return b*F/(b*F+1)  
-    def lnK(G,N):
-        return -G*N/RT
-    def Micelleconc(T,F,N):
+    
+    def Micelleconc(T,F,N): #Micelle concentration
         return (T-F)/N   
-    def Balance(N, G, RT, T,F): 
-        return N*np.log(F)-(G*N/RT)-np.log(T-F)+np.log(N)
-    def plotST(F,TS):
-        return plt.plot(F,TS,'b')  
-    def plotTheta(F,Thet):
-        return plt.plot(F,Thet,'b')
-#Free surfactant concentration 
-#Totalc=conc
-F=[]
-y=0
-for i in Totalc:
-    l=y
-    p=i    
-    func=lambda F: N*np.log(F)-(Gibbs*N)/RT-np.log(i-F)+np.log(N)
-    y=scipy.optimize.bisect(func, l, p)
-    F.append(y)
-F=np.array(F)
-# GET Micelle and Monomer concentration      
-M=STAND.Micelleconc(Totalc,F,N)
-#Surface tension  (Langmuirs EoS and isotherm Free surfactant)
-TS=STAND.SEoSLang(F,Lang,G)
-Theta=STAND.Langiso(F,Lang)
-#logK=STAND.lnK(Gibbs,N)
+    
+    def Balance(NA,GE,T):    # Balance equation
+        def FS(F):
+            return NA*np.log(F)-(GE*NA/RT)-np.log(T-F)+np.log(NA)
+        return FS
 
-#Objective function
-#def OF(Teor,Exp,Uncer,N,Gibbs,Amin,Lang
-#       ):
-#    lambda F: N*np.log(F)-(Gibbs*N)/RT-np.log(i-F)+np.log(N)
-#    return np.sqrt((Teor-Exp)**2/Uncer**2)
-#    
-#Val=np.array([Gibbs,N,Lang,G])    
-#FOev=OF(TS,TS_exp,u_TS)
+    def FreeConc(NA,GE,x):  #Free surfactant concentration
+        F=[]
+        y=0
+        for i in x:
+            l=y
+            p=i
+            q=STAND.Balance(NA,GE,i) 
+            y=scipy.optimize.bisect(q, l, p)
+            F.append(y)
+        F=np.array(F)
+        return F
+    
+    def OF(Teor,Exp,Uncer): #Objective function
+        s=0
+        Xi=((Teor-Exp)/Uncer)**2
+        for i in Xi:
+            s=s+i
+        return  np.sqrt(s)/(len(Exp)-4)
+    
+    def Adjust(Param,conc):
+        
+        G=(10**16/NA)*Tem*(R*10**7)*1/Param[3]
+        F=np.array(STAND.FreeConc(Param[1],Param[0], conc) )      
+        M=np.array(STAND.Micelleconc(conc,F,Param[1])) #Micelle concentration
+        TS=np.array(STAND.SEoSLang(F,Param[2],G)) #Surface tension  (Langmuirs EoS and isotherm)
+        Theta=np.array(STAND.Langiso(F,Param[2])  ) 
+        Xi_sq=STAND.OF(TS,TS_exp,u_TS)
+ 
+        return F,M,TS,Theta,Xi_sq
+
+#Optimization        
+Ps=np.array([Gibbs,N,Lang,MinA])    
+#cmc=np.exp(Ps[0]/RT)
+
+F,M,TS,Theta,Xi_sq=STAND.Adjust(Ps,conc)
+
+print(Xi_sq)
 
 
+#Graphs
 plt.figure(1)
-plt.plot(Totalc,F,'b' ,label='Free surfactant concentration')
-plt.plot(Totalc,M,'y' ,label='Micelle concentration')
+plt.plot(conc,F,'b' ,label='Free surfactant concentration')
+plt.plot(conc,M,'y' ,label='Micelle concentration')
 plt.legend(loc="center right") 
 plt.xlabel('Total concentration/M')
 plt.ylabel('Concentracion/M')
 
 
 plt.figure(2)
-STAND.plotST(Totalc,TS)
+plt.plot(conc,TS)
 plt.plot(conc,TS_exp,'ro')
 plt.xlabel('Free surfactant concentration/M')
 plt.ylabel(r'$\sigma /dinacm^{-1}$')
