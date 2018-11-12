53a39ee8a3e52b6e7a1a260a4d23ae67039feeeb
Mario A. Alanis Garcia <44460940+kerhnewek@users.noreply.github.com>
2018-11-12 18:46:41 +0100

    Add files via upload

--- STAND_MODEL.py
+++ STAND_MODEL.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
+333# -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.optimize
 import json
+import scipy.optimize
 from scipy.optimize import leastsq
+from scipy.optimize import basinhopping
+#from lmfit import minimize, Parameters
 
 plt.close("all")
 
@@ -59,68 +61,93 @@ class STAND:
     def Langiso(F,b): #Langmuir's Isotherm
         return b*F/(b*F+1)  
     
-    def Micelleconc(T,F,N): #Micelle concentration
+    def Micelleconc(T,F): #Micelle concentration
         return (T-F)  
     
     def Balance(NA,GE,T):    # Balance equation
         def FS(F):
-            return NA*np.log(F)-(GE*NA/RT)-np.log(T-F)+np.log(NA)
-        return FS
+            d=NA*np.log(F)-(GE*NA/RT)-np.log(T-F)+np.log(NA)
+            return d   
+        g=FS
+        return g
 
-    def FreeConc(NA,GE,x):  #Free surfactant concentration
+    def FreeConc(NA,GE,conc):  #Free surfactant concentration
         F=[]
         y=0
-        for i in x:
-            l=y
-            p=i
+        for i in conc:
+            p=y
+            l=i
             q=STAND.Balance(NA,GE,i) 
             y=scipy.optimize.bisect(q, l, p)
             F.append(y)
         F=np.array(F)
         return F
-
     
     def Adjust(Param,conc,data,uncer):
         
-        F=STAND.FreeConc(Param[1],Param[0], conc)  
-        M=STAND.Micelleconc(conc,F,Param[1]) #Micelle concentration
-        TS=STAND.SEoSLang(F,Param[2],Param[3]) #Surface tension  (Langmuirs EoS and isotherm)
+        F=STAND.FreeConc(Param[3],Param[0], conc)  
+        M=STAND.Micelleconc(conc,F)
+        TS=STAND.SEoSLang(F,Param[1],Param[2]) 
         Theta=STAND.Langiso(F,Param[2])  
  
-        return F,M,TS,Theta,Xi_sq
+        return F,M,TS,Theta
 
     def Residual(Param,conc,data,uncer):
-        TS=STAND.SEoSLang(STAND.FreeConc(Param[1],Param[0], conc),Param[2],Param[3])
-        return ((TS-data)/uncer)**2
-
+        print(Param)
+        F=STAND.FreeConc(Param[3],Param[0], conc)
+        TS=STAND.SEoSLang(F,Param[1],Param[2])
+#        s=0
+        X=((TS-data)/uncer)**2
+#        for i in X:
+#            s=s+i
+        return np.sqrt(X)
 
 #Curve fitting 
-        
 
-Ps=np.array([Gibbs,N,Lang,MinA])    
+Ps=np.array([Gibbs,Lang,MinA,N])
 #cmc=np.exp(Ps[0]/RT)
-vars = Ps
-out = leastsq(STAND.Residual, vars, args=(conc, TS_exp, u_TS))
+
+def func(Ps_i):
+    NAg=float(Ps_i[0])
+    ELG=float(Ps_i[1])
+    beta=float(Ps_i[2])
+    Amin=float(Ps_i[3])
+    print(Ps_i)
+    F=STAND.FreeConc(NAg,ELG, conc)
+    TS=STAND.SEoSLang(F,beta,Amin)
+    s=0
+    X=((TS-TS_exp)/u_TS)**2
+    for i in X:
+       s=s+i
+    return s
+
+minimizer_kwargs = {"method": "BFGS"}
+ret = basinhopping(func, Ps, minimizer_kwargs=minimizer_kwargs)
+
+seeds= np.array([ret.x[0],ret.x[1],ret.x[2],ret.x[3]])
+
+out=leastsq(STAND.Residual, seeds, args=(conc, TS_exp, u_TS))
+
+
 c=np.array(out)
 d=c[0]
-F,M,TS,Theta,Xi_sq=STAND.Adjust(d,conc,TS_exp,u_TS)
-
-
-
-
-
-
+print(d)
+F,M,TS,Theta=STAND.Adjust(d,conc,TS_exp,u_TS)
+F1,M1,TS1,Theta1=STAND.Adjust(Ps,conc,TS_exp,u_TS)
+print(d)
+print(STAND.Residual(d,conc,TS_exp,u_TS))
 #Graphs
 plt.figure(1)
 plt.plot(conc,F,'b' ,label='Free surfactant')
 plt.plot(conc,M,'y' ,label=' Surfactant molecules in micelles')
 plt.legend(loc="center right") 
 plt.xlabel('Total concentration/M')
-plt.ylabel('Concentracion/M')
+plt.ylabel('Concentration/M')
 
 
 plt.figure(2)
 plt.plot(conc,TS)
+#plt.plot(conc,TS1)
 plt.plot(conc,TS_exp,'ro')
 plt.xlabel('Free surfactant concentration/M')
 plt.ylabel(r'$\sigma /dinacm^{-1}$')
